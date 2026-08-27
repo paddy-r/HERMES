bc1b57f0691009826e64e3210dafc51156eda06c
paddy-r <prehr@leeds.ac.uk>
2026-08-27 17:39:58 +0100

    New EducationRegression for applying logistic regression model from hermes-regress

--- src/hermes/transitions/education.py
+++ src/hermes/transitions/education.py
@@ -188,3 +188,94 @@ class EducationLogit(TransitionModel):
             + 1
         )
 
+
+class EducationRegression(TransitionModel):
+
+    creates_domains = {
+        "life_status": "alive"
+    }
+
+    def apply_transition(
+        self,
+        population
+    ):
+
+        alive = (
+            population.data["life_status"]
+            == "alive"
+        )
+
+        education = (
+            population.data.loc[
+                alive,
+                "education"
+            ]
+        )
+
+        eligible = (
+                education != "na"
+        )
+
+        education = education.loc[
+            eligible
+        ].astype(int)
+
+        progress_eligible = (
+                education < 9
+        )
+
+        indices = (
+            education.index[
+                progress_eligible
+            ]
+        )
+
+        predictor_names = (
+            self.rate_source.predictors
+        )
+
+        predictor_data = (
+            population.data.loc[
+                indices,
+                predictor_names
+            ]
+        )
+
+        progression_probability = (
+            self.rate_source
+            .predict_proba(
+                predictor_data.to_numpy()
+            )[:, 1]
+        )
+
+        print(
+            f"Mean progression probability: "
+            f"{progression_probability.mean():.4f}"
+        )
+
+        u = np.random.random(
+            size=len(indices)
+        )
+
+        progress = (
+            u < progression_probability
+        )
+
+        print(
+            f"Education progression: "
+            f"{progress.sum()}"
+        )
+
+        progressed_indices = (
+            indices[progress]
+        )
+
+        population.data.loc[
+            progressed_indices,
+            "education"
+        ] = (
+            education.loc[
+                progressed_indices
+            ]
+            + 1
+        )
