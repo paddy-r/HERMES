eaee77eb90d72cf994dbb823009d96fa97637a76
paddy-r <prehr@leeds.ac.uk>
2026-08-27 17:38:32 +0100

    Cleverer metadata inference in regression source handler

--- src/hermes/sources/regression.py
+++ src/hermes/sources/regression.py
@@ -2,10 +2,31 @@ import os
 import numpy as np
 import pandas as pd
 import pickle
+import json
 from hermes.sources.base import Source
-
+from hermes.constants import REGRESSION_LAG
 
 class RegressionSource(Source):
+
+    @property
+    def predictors(self):
+        return self.metadata.get(
+            "predictors",
+            []
+        )
+
+    @property
+    def response(self):
+        return self.metadata.get(
+            "response"
+        )
+
+    @property
+    def lag(self):
+        return self.metadata.get(
+            "lag",
+            REGRESSION_LAG
+        )
 
     def __init__(
         self,
@@ -27,14 +48,14 @@ class RegressionSource(Source):
 
         if "model_path" in config:
 
-            fullpath = os.path.join(
+            basepath = os.path.join(
                 config["model_path"],
                 config["model"]
             )
 
         else:
 
-            fullpath = os.path.join(
+            basepath = os.path.join(
                 self.inpath,
                 "regressions",
                 config["model"]
@@ -42,14 +63,29 @@ class RegressionSource(Source):
 
         print(
             f"Resolved model path:\n"
-            f"{fullpath}"
+            f"{basepath}"
+        )
+
+        model_path = (
+                basepath + ".pkl"
+        )
+
+        metadata_path = (
+                basepath + ".json"
         )
 
         with open(
-                fullpath,
+                model_path,
                 "rb"
         ) as f:
             self.model = pickle.load(
+                f
+            )
+
+        with open(
+                metadata_path
+        ) as f:
+            self.metadata = json.load(
                 f
             )
 
@@ -60,6 +96,14 @@ class RegressionSource(Source):
         print(
             self.model,
             self.model.__class__.__name__
+        )
+
+        print(
+            "Loaded regression metadata:"
+        )
+
+        print(
+            self.metadata
         )
 
     def verify(self):
@@ -74,6 +118,14 @@ class RegressionSource(Source):
     ):
 
         return self.model.predict(
+            X
+        )
+
+    def predict_proba(
+            self,
+            X
+    ):
+        return self.model.predict_proba(
             X
         )
 
